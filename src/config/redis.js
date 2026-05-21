bd5327c335db736269fe73ef3aab843f8a309c77
Darshan Gupta <darshangupta78421@gmail.com>
2026-05-21 20:37:32 +0530

    Added TLS checking for upstash

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,6 +6,8 @@ dotenv.config();
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
 });
 
 redis.on('connect', () => {
